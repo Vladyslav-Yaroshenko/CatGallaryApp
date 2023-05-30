873e024930c3623d90ca2334d07267f0023f58cd
Vladyslav-Yaroshenko <vlad.yaroshenko7@gmail.com>
2023-05-30 14:56:31 +0300

    setup views

--- CatGallaryApp/DetailModule/View/DetailViewController.swift
+++ CatGallaryApp/DetailModule/View/DetailViewController.swift
@@ -9,35 +9,57 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    
-    @IBOutlet weak var imageView: UIImageView!
+    var imageView: UIImageView!
+    var shareButton: UIButton!
     
     var presenter: DetailViewPresenterProtocol!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupImageView()
+        setupShareButton()
         presenter.setPhoto()
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupImageView() {
+        let width = view.frame.width
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: width))
+        imageView.center = view.center
+        view.addSubview(imageView)
     }
-    */
+    
+    private func setupShareButton() {
+        let color = UIColor { traitCollection -> UIColor in
+            return traitCollection.userInterfaceStyle == .dark ? .white : .black
+        }
+        shareButton = UIButton(type: .system)
+        shareButton.translatesAutoresizingMaskIntoConstraints = false
+        shareButton.setTitle("Tap to share", for: .normal)
+        shareButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        shareButton.setTitleColor(color, for: .normal)
+        shareButton.tintColor = color
+        
+        view.addSubview(shareButton)
+        
+        NSLayoutConstraint.activate([
+            shareButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: interitemSpacing),
+            shareButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        shareButton.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func shareButtonTapped() {
+            
+    }
 
 }
 
 extension DetailViewController: DetailViewProtocol {
     func setCatPhoto(image: CatPhoto?) {
         self.imageView.image = image?.image
-        self.imageView.contentMode = .scaleAspectFill
+        self.imageView.contentMode = .scaleAspectFit
     }
-    
-    
 }
